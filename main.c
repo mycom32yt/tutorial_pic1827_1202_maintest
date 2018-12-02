a3d34712730b4312c8f661f1d180015899fcfa44
y <tt>
2018-12-02 11:30:05 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Y
+ *
+ * Created on 2018/12/02, 11:08
+ */
+
+
+#include <xc.h>
+
+#pragma config FOSC = INTOSC, WDTE = OFF, PWRTE = ON,MCLRE = OFF, CP = OFF
+
+void main(void) {
+    return;
+}
